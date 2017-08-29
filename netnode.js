eb1600667d9fe1d19f203e6acd4c26ac5bc810ba
Dmitry Gusarov <dmitry.gusarov@gmail.com>
2017-08-29 11:13:14 +0300

    add net note

---
+++ netnode.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var net = require('net');
+var JsonSocket = require('json-socket');
+
+class NetNode {
+	constructor(port) {
+		this.port = port;
+		this.server = null;
+	}
+
+	start() {
+		this.server = net.createServer();
+		this.server.listen(port == null ? 7770 : port);
+		this.server.on('connection', function(socket) {
+			socket = new JsonSocket(socket);
+			socket.on('message', function(message) {
+				if (message.command == 'a') {
+				} else if (message.command == 'b') {
+				}
+			});
+		});
+	}
+}
+
+module.exports = {
+	NetNode: NetNode
+};
